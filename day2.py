15ee8381b8170d8c573d1d23bdbcb6d39cb4340b
phanindra143457 <jagannadhamphanindra93@gmail.com>
2026-01-22 13:57:21 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,19 @@
+decimal_val=10.25
+name ="phanindra kumar"
+age =22
+list=[1,2,"2",4,6,3,7]
+tuple=(3,55,645,42,[55,99,57],66)
+set={1,3,4,67,98,24,90}
+dict={"a":55,57:"phanindra kumar",678:789}
+cards={"a","hello","phanindra kumar","hi",635,8,67}
+student_marks={"math":90,"tel":80,"eng":80,"soc":75}
+weight=54.5
+
+print("my name is:",name)
+print("my age is:",age)
+print("my weight:",weight)
+print(tuple,tuple)
+print(set,set)
+print(dict)
+print(cards)
+print("list of student_marks:",student_marks)
